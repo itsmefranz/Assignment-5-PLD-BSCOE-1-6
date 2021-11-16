d0299dbf57191d09441674f7ce54fb94e68694ad
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-16 23:55:58 +0800

    rearranged indents and order of grade system and added grade and description for 94-100

--- program1.py
+++ program1.py
@@ -2,10 +2,19 @@ def gradePercentage():
     percentage= float(input("Please indicate students' percentage: "))
     return percentage
 
-corres= gradePercentage()
+grade= gradePercentage()
 
-if grade >= 65 and  grade <= 74:
-    print("Grade/Mark: 5.0")
-    print("Description: Failure")
-elif grade == 75:
-    print("Passing")
+if grade >= 97 and grade == 100:
+    print("Grade/Mark: 1.0")
+    print("Description: Excellent")
+else:
+    if grade >= 94 and grade <= 96:
+        print("Grade/Mark: 1.25")
+        print("Description: Excellent")
+    else:
+        if  grade >= 65 and  grade <= 74:
+            print("Grade/Mark: 5.0")
+            print("Description: Failure")
+        else:
+            if grade == 75:
+                print("Passing")
