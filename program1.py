e190feb9ac3706e984cf80544386374b52101b49
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-16 15:45:54 +0800

    added passing grade

--- program1.py
+++ program1.py
@@ -1,5 +1,7 @@
 
 grade= int(input("Please indicate student grade: "))
 
-if grade > 64 and  grade <= 74:
+if grade >= 64 and  grade <= 74:
     print("Failure")
+elif grade == 75:
+    print("Passing")
