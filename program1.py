f815bb3fb7d661267704654c7f12ac47b2368bda
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-16 23:33:12 +0800

    added print mark and description in if statement for grade 65 and 74

--- program1.py
+++ program1.py
@@ -1,9 +1,11 @@
 def gradePercentage():
-    percentage= float(input("Please indicate students' grade: "))
+    percentage= float(input("Please indicate students' percentage: "))
+    return percentage
 
 corres= gradePercentage()
 
-if grade >= 64 and  grade <= 74:
-    print("Failure")
+if grade >= 65 and  grade <= 74:
+    print("Grade/Mark: 5.0")
+    print("Description: Failure")
 elif grade == 75:
     print("Passing")
