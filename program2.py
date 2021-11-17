5902817deddaeac472bf2d94b30be76600dc925c
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-17 09:51:02 +0800

    added comments for further understanding of the program

--- program2.py
+++ program2.py
@@ -1,9 +1,12 @@
+#Program 2: Find the lowest number
+#Create a program that ask 3 numbers.
 a= int(input("Enter first number: "))
 b= int(input("Enter second number: "))
 c= int(input("Enter third number: "))
 
 smallest= 0
 
+#Find the lowest number using only if-else statement.
 if a<b and a<c:
     smallest= a
 else:
@@ -13,4 +16,5 @@ else:
         if c<a and c<b:
             smallest= c
 
+#Display the lowest number
 print(f'{smallest}, is the smallest of the three numbers.')
