d7a74f0fa7b1a31602619baa49e7a35c7caa2757
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-20 17:16:12 +0800

    added if statement for Teen

--- program3.py
+++ program3.py
@@ -2,3 +2,9 @@ age= int(input("Age: "))
 
 if age > -1 and age <= 12:
     print("Kid")
+else:
+    if age >= 13 and age <= 17:
+        print("Teen")
+    else:
+        if age== 18:
+            print("")
