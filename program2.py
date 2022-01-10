fb90398b19c91aaa2537cbb55db6c02f2183c425
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2022-01-10 14:24:23 +0800

    revised the variables, replaced them with words instead

--- program2.py
+++ program2.py
@@ -1,20 +1,20 @@
 #Program 2: Find the lowest number
 #Create a program that ask 3 numbers.
-a= int(input("Enter first number: "))
-b= int(input("Enter second number: "))
-c= int(input("Enter third number: "))
+first_num= int(input("Enter first number: "))
+second_num= int(input("Enter second number: "))
+third_num= int(input("Enter third number: "))
 
 smallest= 0
 
 #Find the lowest number using only if-else statement.
-if a<b and a<c:
-    smallest= a
+if first_num < second_num and first_num < third_num:
+    smallest= first_num
 else:
-    if b<a and b<c:
-        smallest= b
+    if second_num< first_num and second_num< third_num:
+        smallest= second_num
     else:
-        if c<a and c<b:
-            smallest= c
+        if third_num<first_num and third_num<second_num:
+            smallest= third_num
 
 #Display the lowest number
 print(f'{smallest}, is the smallest of the three numbers.')
